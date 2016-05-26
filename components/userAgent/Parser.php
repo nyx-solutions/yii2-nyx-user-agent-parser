ce8921e204a669c2e6138972093042173296ac5f
Jonatas Sas <atendimento@jsas.com.br>
2016-05-26 09:36:50 -0300

    1.0.1

--- components/userAgent/Parser.php
+++ components/userAgent/Parser.php
@@ -2,11 +2,27 @@
 
     namespace nox\components\userAgent;
 
+    use yii\base\Component;
+
     /**
      * Class Parser
      *
      * @package nox\components
      */
-    class Parser {
+    class Parser extends Component
+    {
+        /**
+         * @var string[]
+         */
+        public $parser;
 
+        /**
+         * @inheritdoc
+         */
+        public function init()
+        {
+            parent::init();
+
+            $this->parser = parse_user_agent();
+        }
     }
